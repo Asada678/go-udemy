c38feeea5bc4c986ad43b0690f24ab70ccc91980
Adada678 <asada.tomoya0923@gmail.com>
2023-11-11 17:57:31 +0900

    closure

---
+++ lesson06/func05.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// closure
+
+func Later() func(string) string {
+	var store string
+	return func(next string) string {
+		{
+			s := store
+			store = next
+			return s
+		}
+	}
+}
+
+func main() {
+	f := Later()
+	fmt.Println(f("Hello"))
+	fmt.Println(f("my"))
+	fmt.Println(f("name"))
+	fmt.Println(f("is"))
+	fmt.Println(f("Golang"))
+}
