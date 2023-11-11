2badebceed8d6fcc9deee84e58361d33a049c29a
asada0923 <146704659+asada0923@users.noreply.github.com>
2023-11-11 11:59:59 +0900

    interface

---
+++ lesson03/07_interface/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// interface
+
+func main() {
+	var x interface{}
+	fmt.Println(x)
+
+	x = 1
+	fmt.Println(x)
+
+	x = 3.14
+	fmt.Println(x)
+
+	x = "A"
+	fmt.Println(x)
+
+	x = false
+	fmt.Println(x)
+
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
+
+	x = 2
+	// fmt.Println(x + 3)
+}
