03409d9617cb9da1656d38851c41f2064ca6bd9d
Asada678 <asada.tomoya0923@gmail.com>
2023-11-26 09:59:47 +0900

    scope

---
+++ lesson12/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	. "fmt"
+	f "fmt"
+
+	"lesson12/foo"
+)
+
+// scope
+
+func appName() string {
+	const AppName = "GoApp"
+	var Version string = "1.0"
+	return AppName + " " + Version
+}
+
+func Do(s string) (b string) {
+	// var b string = s
+	b = s
+	{
+		b:= "BBBB"
+		fmt.Println(b)
+	}
+	fmt.Println(b)
+	return b
+}
+
+func main() {
+	fmt.Println(foo.Max)
+	// f.Println(foo.min) // 定義された変数が小文字から始まる場合、外部から参照できない
+
+	f.Println(foo.ReturnMin()) // 関数が呼び出しているパッケージ内の小文字変数は参照できる
+	Println(foo.Max)
+
+	fmt.Println(appName())
+	// fmt.Println(AppName, Version)]
+
+	fmt.Println(Do("AAA"))
+}
