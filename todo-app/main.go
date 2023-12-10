8457d570fb512321fe050e03bac70b6a51b13ac6
Adada678 <asada.tomoya0923@gmail.com>
2023-12-10 11:27:47 +0900

    (todo-app)create todo

--- todo-app/main.go
+++ todo-app/main.go
@@ -15,27 +15,28 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	//u := &models.User{}
-	//u.Name = "test"
-	//u.Email = "test@example.com"
-	//u.Password = "testtest"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	err := u.CreateUser()
+	if err != nil {
+		return
+	}
+
+	//
+	//u.Name = "Test2"
+	//u.Email = "test2@example.com"
+	//u.UpdateUser()
+	//u, _ = models.GetUser(1)
 	//fmt.Println(u)
 	//
-	//err := u.CreateUser()
-	//if err != nil {
-	//	return
-	//}
+	//u.DeleteUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
